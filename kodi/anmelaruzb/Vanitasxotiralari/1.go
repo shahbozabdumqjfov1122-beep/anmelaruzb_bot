a2545fa8f240d7d2e1a57f22b45e61089e45de69
shahboz <shahbozabdumqjfov1122@gmail.com>
2025-09-20 14:36:10 +0500

    izoy yozishga erindim.

--- kodi/anmelaruzb/Vanitasxotiralari/1.go
+++ kodi/anmelaruzb/Vanitasxotiralari/1.go
@@ -7,7 +7,7 @@ import (
 func Home(c tele.Context) error {
 	b := c.Bot()
 	channelID := int64(-1003073655363)
-	messageIDs := []int{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13}
+	messageIDs := []int{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
 
 	for _, msgID := range messageIDs {
 		msg := &tele.Message{
