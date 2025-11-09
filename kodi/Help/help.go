69c7e28416b6c762979c48a99d2a22151568b1dd
shahboz <shahbozabdumqjfov1122@gmail.com>
2025-11-09 07:47:00 +0500

    izoy yozishga erindim.

--- kodi/Help/help.go
+++ kodi/Help/help.go
@@ -10,7 +10,12 @@ func Home(c tele.Context) error {
 	switch text {
 
 	case "🧩 help", "/help":
-		return c.Send("biz bot ham yaratamiz..\nadmin---@TM_ESPORTS\nadmin---@TM_ESPORTS2")
+		return c.Send("🧠 *Yordam bo‘limi*\n\n" +
+			"Bizning bot turli xizmatlarni taqdim etadi va sizga yordam berishga tayyor!\n\n" +
+			"📞 *Agar muammo yoki takliflaringiz bo‘lsa*, quyidagi adminlar bilan bog‘laning:\n" +
+			"👤 @TM_ESPORTS\n" +
+			"👤 @Animelaruzb_admin\n\n" +
+			"✨ Har doim siz bilan birga — *Biz bot jamoasi!* 🤖")
 
 	default:
 		return c.Send("Noma'lum buyruq. Iltimos, menyudan foydalaning.")
