6669c3103001f5c228c3dc7915d5974cae309586
shahboz <shahbozabdumqjfov1122@gmail.com>
2025-12-12 10:16:42 +0500

    izoy yozishga erindim.

--- kodi/anmelaruzb/GachiakutaQasos/1.go
+++ kodi/anmelaruzb/GachiakutaQasos/1.go
@@ -7,20 +7,24 @@ import (
 func Home(c tele.Context) error {
 	b := c.Bot()
 	channelID := int64(-1003100560100)
-	messageIDs := []int{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+	messageIDs := []int{}
+	for i := 2; i < 50; i++ {
+		messageIDs = append(messageIDs, i)
+	}
 
 	for _, msgID := range messageIDs {
+
 		msg := &tele.Message{
 			ID:   msgID,
 			Chat: &tele.Chat{ID: channelID},
 		}
 
-		// Xabarni forward emas, copy qilish (faqat video chiqadi, yozuvsiz)
 		_, err := b.Copy(c.Sender(), msg)
 		if err != nil {
-			return c.Send("❌ Video olishda xatolik: " + err.Error())
+			// ❌ Xato bo‘lsa o‘sha xabarni tashlab keyingisiga o‘tadi
+			continue
 		}
 	}
 
-	return nil
+	return c.Send("✅ Videolar yuborish tugadi")
 }
