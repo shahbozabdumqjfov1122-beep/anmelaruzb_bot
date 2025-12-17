b4266538869e14c84a6765855a99f33cf3e3c524
shahboz <shahbozabdumqjfov1122@gmail.com>
2025-12-17 18:40:39 +0500

    izoy yozishga erindim.

--- kodi/anmelaruzb/Shangrilachegarasi/1.go
+++ kodi/anmelaruzb/Shangrilachegarasi/1.go
@@ -8,7 +8,7 @@ func Home(c tele.Context) error {
 	b := c.Bot()
 	channelID := int64(-1003407535551)
 	messageIDs := []int{}
-	for i := 2; i < 30; i++ {
+	for i := 2; i < 60; i++ {
 		messageIDs = append(messageIDs, i)
 	}
 
